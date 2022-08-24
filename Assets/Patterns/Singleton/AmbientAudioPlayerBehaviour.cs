5a13a101538702734f7c1f09a78dd2d7e039d31d
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2022-08-24 21:18:45 +0700

    Singleton with Unity

--- Assets/Patterns/Singleton/AmbientAudioPlayerBehaviour.cs
+++ Assets/Patterns/Singleton/AmbientAudioPlayerBehaviour.cs
@@ -1,12 +1,32 @@
 using UnityEngine;
 
-public class AmbientAudioPlayerBehaviour : MonoBehaviour {
-    private void Awake() {
-        DontDestroyOnLoad(gameObject);
-    }
+public class AmbientAudioPlayerBehaviour : MonoBehaviour
+{
+    #region --Properties-- (Auto)
+    public static AmbientAudioPlayerBehaviour Instance { get; private set; }
+    #endregion
 
+
+
+    #region --Methods-- (Built In)
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+    #endregion
+
+
+
+    #region --Methods-- (Custom PUBLIC)
     public void FadeNewMusic(AudioClip clip)
     {
         // TODO
     }
+    #endregion
 }
--- Assets/Patterns/Singleton/SceneReloader.cs
+++ Assets/Patterns/Singleton/SceneReloader.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneReloader : MonoBehaviour {
-    private void Update() {
+public class SceneReloader : MonoBehaviour
+{
+    #region --Methods-- (Built In)
+    private void Update()
+    {
         if (Input.GetKeyDown(KeyCode.R))
         {
             Scene scene = SceneManager.GetActiveScene();
             SceneManager.LoadScene(scene.name);
         }
     }
+    #endregion
 }
