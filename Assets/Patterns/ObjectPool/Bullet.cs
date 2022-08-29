a5a6240c8e2f5ce64d8ef361f3c7d133f2ea219a
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2022-08-30 00:06:53 +0700

    Object Pooling with Unity Built In

--- Assets/Patterns/ObjectPool/Bullet.cs
+++ Assets/Patterns/ObjectPool/Bullet.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Bullet : MonoBehaviour
 {
@@ -8,15 +9,30 @@ public class Bullet : MonoBehaviour
 
 
 
+    #region --Fields-- (In Class)
+    private IObjectPool<Bullet> _bulletPool;
+    #endregion
+
+
+
     #region --Methods-- (Built In)
     private void Update()
     {
         transform.position += _speed * Time.deltaTime;
     }
-    
+
     private void OnBecameInvisible()
     {
-        Destroy(gameObject);
+        _bulletPool.Release(this);
+    }
+    #endregion
+
+
+
+    #region --Methods-- (Custom PUBLIC)
+    public void Setup(IObjectPool<Bullet> objectPool)
+    {
+        _bulletPool = objectPool;
     }
     #endregion
 }
--- Assets/Patterns/ObjectPool/Launcher.cs
+++ Assets/Patterns/ObjectPool/Launcher.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Launcher : MonoBehaviour
 {
@@ -8,13 +9,55 @@ public class Launcher : MonoBehaviour
 
 
 
+    #region --Properties-- (Auto)
+    public IObjectPool<Bullet> BulletPool { get; private set; }
+    #endregion
+
+
+
     #region --Methods-- (Built In)
+    private void Awake()
+    {
+        BulletPool = new ObjectPool<Bullet>(CreatePoolItem, OnTakeFromPool, OnReturnedToPool, OnDestroyItem, collectionCheck:true, defaultCapacity:10, maxSize:3);
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(_bulletPrefab);
+            BulletPool.Get();
         }
+    }
+    #endregion
+
+
+
+    #region --Methods-- (Subscriber) ~ObjectPool~
+    private Bullet CreatePoolItem()
+    {
+        Bullet bullet = Instantiate(_bulletPrefab);
+        bullet.Setup(BulletPool);
+
+        return bullet;
+    }
+
+    private void OnTakeFromPool(Bullet bullet)
+    {
+        print("TAKE FROM POOL");
+        bullet.gameObject.SetActive(true);
+        bullet.transform.position = transform.position; // set back to Launcher position
+    }
+
+    private void OnReturnedToPool(Bullet bullet)
+    {
+        print("RETURNED");
+        bullet.gameObject.SetActive(false);
+    }
+
+    // If the pool capacity is reached then any items returned will be destroyed, but will create more if needed. For Resources Saving Purpose, it needs memory for keeping items around.
+    private void OnDestroyItem(Bullet bullet)
+    {
+        Destroy(bullet.gameObject);
     }
     #endregion
 }
